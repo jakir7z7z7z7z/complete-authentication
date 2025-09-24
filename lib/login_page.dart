9535784f19746489f4eae8e0aa63e624c6934680
Md.Jakir Hossain <jakir7z7z7z7z@gmail.com>
2025-09-24 14:46:30 +0600

    doing login interface

---
+++ lib/login_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white10,
+        body: Column(
+
+          //hello again
+
+          //email textfield
+
+          // pass textfield
+
+          //sign in button
+
+
+          //register now
+
+        ),
+      ),
+    );
+  }
+}
